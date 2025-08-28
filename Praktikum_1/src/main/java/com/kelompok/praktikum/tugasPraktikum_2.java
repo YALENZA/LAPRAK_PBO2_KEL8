c02e7b8d07ce601edc19392964cef038a892f4cb
Athaya Nabil Putra Halby <araara91pku@gmail.com>
2025-08-28 14:50:42 +0700

    Add files via upload

---
+++ Praktikum_1/src/main/java/com/kelompok/praktikum/tugasPraktikum_2.java
@@ -0,0 +1,24 @@
+package com.kelompok.praktikum;
+
+public class tugasPraktikum_2 {
+    public static void main (String[] args){
+        //        Menginisialisasi variabel
+        String nama = "Athaya Nabil Putra Halby", alamat = "Jalan Sei Mintan";
+        long nim = 2407134906L;
+        final String GolonganDarah = "-";
+        double[] ipSemester = {3.65, 3.54, 4.0};
+        int umur = 19;
+        byte umurDiperkecil = (byte) umur;
+        
+        //      Menampilkan Biodata
+        System.out.println("Nama saya = " + nama);
+        System.out.println("NIM = " + nim);
+        System.out.println("IP Semester 1 = " + ipSemester[0]);
+        System.out.println("IP Semester 2 = " + ipSemester[1]);
+        System.out.println("IP Semester 3 = " + ipSemester[2]);
+        System.out.println("Alamat = " + alamat);
+        System.out.println("Umur = " + umurDiperkecil);
+        System.out.println("Golongan Darah = " + GolonganDarah);
+
+    }
+}
