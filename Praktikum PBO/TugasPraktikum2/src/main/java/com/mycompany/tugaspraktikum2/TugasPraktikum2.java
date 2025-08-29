2a643f20526a68039e1906e8a050be008e275af5
RifqiFath <mrifqi.fath@gmail.com>
2025-08-29 13:16:16 +0700

    Add files via upload

---
+++ Praktikum PBO/TugasPraktikum2/src/main/java/com/mycompany/tugaspraktikum2/TugasPraktikum2.java
@@ -0,0 +1,34 @@
+package com.mycompany.tugaspraktikum2;
+
+/**
+ *
+ * @author Rifqi
+ *         2407114071
+ */
+public class TugasPraktikum2 {
+
+    public static void main(String[] args) {
+        
+        String nama ="Muhammad Rifqi Fathurrahman";
+        String alamat = "Jalan Bangau Sakti";
+        String golDarah ="AB";
+        int umur;
+        umur = 20;
+        long nim = 2407114071L;
+        double []ipsemester = {3.9 , 3.7 , 3.8};
+        final double PI = 3.14;
+        byte semesterByte = (byte)ipsemester[0];
+        
+        System.out.println("Nama Saya : " + nama);
+        System.out.println("NIM Saya : " + nim);
+        System.out.println("Umur Saya : " + umur);
+        System.out.println("Alamat Saya : " + alamat);
+        System.out.println("Golongan Darah Saya : " + golDarah);
+        System.out.println("IP Semester 1 saya : " + ipsemester[0]);
+        System.out.println("IP Semester 2 saya : " + ipsemester[1]);
+        System.out.println("IP Semester 3 saya : " + ipsemester[2]);
+        System.out.println("Nilai PI : " + PI);
+        System.out.println("Byte Semester 1 saya : " + semesterByte );
+        
+    }
+}
