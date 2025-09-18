1fecd3b75a91ad09e1b83045738d8afde79b89b2
RifqiFath <mrifqi.fath@gmail.com>
2025-09-18 09:35:48 +0700

    Add files via upload

---
+++ TugasPraktikum5/src/main/java/com/mycompany/tugaspraktikum5/TokoElektronik.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugaspraktikum5;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TokoElektronik {
+    String namaProduk;
+    String merek;
+    double harga;
+    
+    public TokoElektronik() {
+        this.namaProduk = "Tidak Dikenal";
+        this.merek = "Tidak Dikenal";
+        this.harga = 0.0;
+    }
+    
+    public TokoElektronik(String namaProduk, String merek) {
+        this.namaProduk = namaProduk;
+        this.merek = merek;
+        this.harga = 0.0;
+    }
+    
+    public TokoElektronik(String namaProduk, String merek, double harga) {
+        this.namaProduk = namaProduk;
+        this.merek = merek;
+        this.harga = harga;
+    }
+    
+    public void tampilkanInfo() {
+        System.out.println("Nama Produk: " + this.namaProduk);
+        System.out.println("Merek: " + this.merek);
+        System.out.println("Harga: Rp" + this.harga);
+    }
+    
+    public void tampilkanInfo(boolean adaDiskon) {
+        tampilkanInfo();
+        if (adaDiskon) {
+            System.out.println("Produk ini sedang dalam promo diskon!");
+        } else {
+            System.out.println("Produk ini tidak sedang diskon.");
+        }
+    }
+}
---
+++ TugasPraktikum5/src/main/java/com/mycompany/tugaspraktikum5/TugasPraktikum5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugaspraktikum5;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TugasPraktikum5 {
+    public static void main(String[] args) {
+        TokoElektronik produkSatu = new TokoElektronik("Laptop", "ASUS", 12500000.0);
+        TokoElektronik produkDua = new TokoElektronik("Smartphone", "Samsung");
+        TokoElektronik produkTiga = new TokoElektronik();
+        
+        System.out.println("--- Produk 1 ---");
+        produkSatu.tampilkanInfo(true);
+        System.out.println("\n--- Produk 2 ---");
+        produkDua.tampilkanInfo();
+        System.out.println("\n--- Produk 3 ---");
+        produkTiga.tampilkanInfo();
+    }
+}
