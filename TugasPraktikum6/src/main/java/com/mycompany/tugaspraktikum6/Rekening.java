cb8d33f001b57b792a1d182b241725c700b0ea85
anggitanatarina <anggitanatarina@gmail.com>
2025-10-02 09:09:57 +0700

    Add files via upload

---
+++ TugasPraktikum6/src/main/java/com/mycompany/tugaspraktikum6/Rekening.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugaspraktikum6;
+
+/**
+ *
+ * @author User
+ */
+public class Rekening {
+    private int saldo;
+    
+    public Rekening(int saldo){
+        this.saldo = saldo;
+    }
+    public int getSaldo(){
+        return saldo;
+    }
+    
+    public void SetSaldo(int saldo){
+        this.saldo = saldo;
+        System.out.println("Saldo dari Bank saat ini : " + saldo);
+    }
+}
