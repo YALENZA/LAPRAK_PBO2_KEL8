006dce1b83ab867aa3136a2981c9c59ea97308f0
RifqiFath <mrifqi.fath@gmail.com>
2025-10-31 12:48:21 +0700

    Add files via upload

---
+++ Praktikum9/src/main/java/com/mycompany/praktikum9/Praktikum9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum9;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Praktikum9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
